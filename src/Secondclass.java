073dad22b9ff29db7a09abede96f204400a95d62
sunkarasai240@gmail.com <sunkarasai240@gmail.com>
2022-06-10 11:58:46 +0530

    first round of commit

---
+++ src/Secondclass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Secondclass {
+	
+	public static void main(String args[]) {
+		
+		Scanner team = new Scanner(System.in);
+		
+		String mytext = team.nextLine();
+		
+		System.out.println(mytext);
+		
+				
+	}
+	
+
+}
